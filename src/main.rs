c23ebc21500e5b3d99d98b89c569c9f6b17504f8
Alberto Coletta <3520636+albertocoletta@users.noreply.github.com>
2025-12-03 16:12:31 +0100

    Improve welcome message

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!(
-        "Welcome to the advent of code 2025! To run a puzzle, use `cargo run --bin <puzzle-name>`."
+        "Welcome to the Advent of Code 2025! To run a puzzle, use `cargo run --bin <puzzle-name>` or `just run <day-number>`."
     );
 }
